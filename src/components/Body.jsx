ca5581f473f76a79f0e1e5c7616bb000de92fd43
NOBISSS <parthchauhan25125.com>
2025-12-25 15:41:06 +0530

    PROFILE

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import NavBar from './NavBar'
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet, useNavigate, useSearchParams } from 'react-router-dom'
 import Footer from './Footer'
-
+import axios from 'axios'
+import { BASE_URL } from '../utils/constants';
+import { useDispatch, useSelector } from 'react-redux'
+import { addUser } from '../utils/userSlice'
 const Body = () => {
+  const dispatch=useDispatch();
+  const navigate=useNavigate();
+  const userData=useSelector((store)=>store.user);
+  const fetchUser=async()=>{
+    try{
+    if(userData){
+      return;
+    }
+    const res=await axios.get(BASE_URL+"/profile/view",{withCredentials:true});
+    dispatch(addUser(res.data.user));
+    }catch(error){
+      if(error.status===401){
+      navigate("/login");
+      }
+      console.log("ERROR:"+error.message);
+    }
+  }
+  useEffect(()=>{
+    fetchUser();
+  },[])
   return (
     <div>
     <NavBar/>
